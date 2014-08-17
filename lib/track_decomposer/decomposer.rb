d6edd2ef4dc86b384d148fb01bed5b727fdd0181
Alexandre Quintino <amsquintino@gmail.com>
2014-08-17 22:34:31 +0200

    Take care of tracks with remixes

--- lib/track_decomposer/decomposer.rb
+++ lib/track_decomposer/decomposer.rb
@@ -15,8 +15,8 @@ module TrackDecomposer
       if @raw.include?("-")
         parts = @raw.split("-")
 
-        @artists = decompose_artists(parts[0])
-        @name, @remixer = decompose_trackname(parts[1])
+        @artists = decompose_artists(parts[0].strip)
+        @name, @remixer = decompose_trackname(parts[1].strip)
       else
         raise UndecomposableTrack.new("Don't know how to decompose a track without \"-\". Track was:#{@raw}")
       end
@@ -24,11 +24,23 @@ module TrackDecomposer
     end
 
     def decompose_artists(artist_part)
-      [artist_part.strip]
+      [artist_part]
     end
 
     def decompose_trackname(trackname_part)
-      trackname_part.strip
+      if /\(.+\)/.match trackname_part
+        match = /(?<name>.*)\((?<remix>.+)\)/.match(trackname_part)
+        name = match[:name].strip
+        remixer = decompose_remix(match[:remix])
+        return name, remixer
+      else
+        trackname_part.strip
+      end
+    end
+
+    def decompose_remix(remix_part)
+      remixer = /(?<remixer>.+)\b(remix|mix)/i.match(remix_part)[:remixer]
+      remixer.strip
     end
   end
 
--- spec/decomposer_spec.rb
+++ spec/decomposer_spec.rb
@@ -1,18 +1,35 @@
 describe TrackDecomposer::Decomposer do
   
-  tracks = ["SIS - Orgsa"]
+  tracks = {
+    "SIS - Orgsa" => { artists: ["SIS"], name: "Orgsa", remixer: nil },
+    "Roy Davis Jr - About Love (Pezzner Remix)" => { artists: ["Roy Davis Jr"], name: "About Love", remixer: "Pezzner" }
+  }
 
   let(:decomposed_track) { TrackDecomposer::Decomposer.do(track) }
 
-  context "#{tracks[0]}" do
-    let(:track) { tracks[0] }
+  tracks.keys.each do |track|
+    context "#{track}" do
+      let(:track) { track }
 
-    it "returns the artist correctly" do
-      expect(decomposed_track.artists.first).to eq("SIS")
+      it "returns the artist correctly" do
+        expect(decomposed_track.artists).to eq(tracks[track][:artists])
+      end
+
+      it "returns the track name correctly" do
+        expect(decomposed_track.name).to eq(tracks[track][:name])
+      end
+
+      it "returns the remixer correctly" do
+        expect(decomposed_track.remixer).to eq(tracks[track][:remixer])
+      end
     end
+  end
 
-    it "returns the track name correctly" do
-      expect(decomposed_track.name).to eq("Orgsa")
+  context "invalid track" do
+    let(:track) { "Invalid Track" }
+
+    it "throws an exception when the track doesn't have \"-\"" do
+      expect{TrackDecomposer::Decomposer.do(track)}.to raise_error(TrackDecomposer::UndecomposableTrack)
     end
   end
 
